b4eadb6e35642c0e86ea82a1508f8e4de54874e6
ngongang <k.ngongang@addon-solution.de>
2024-07-10 15:00:50 +0200

    chore: Add Vite configuration, Redux store, gitignore, and ESLint config

---
+++ src/venueSlice.js
@@ -0,0 +1,62 @@
+// venueSlice.js
+import { createSlice } from "@reduxjs/toolkit";
+
+export const venueSlice = createSlice({
+  name: "venue",
+  initialState: [
+    {
+      img: "src/assets/conference-room.jpg",
+      name: "Conference Room (Capacity:15)",
+      cost: 3500,
+      quantity: 0,
+    },
+    {
+      img: "src/assets/auditorium.jpg",
+      name: "Auditorium Hall (Capacity:200)",
+      cost: 5500,
+      quantity: 0,
+    },
+    {
+      img: "src/assets/presentation-room.jpg",
+      name: "Presentation Room (Capacity:50)",
+      cost: 700,
+      quantity: 0,
+    },
+    {
+      img: "src/assets/meeting-room.jpg",
+      name: "Large Meeting Room (Capacity:10)",
+      cost: 900,
+      quantity: 0,
+    },
+    {
+      img: "src/assets/small-meeting-room.jpg",
+      name: "Small Meeting Room (Capacity:5)",
+      cost: 1100,
+      quantity: 0,
+    },
+  ],
+  reducers: {
+    incrementQuantity: (state, action) => {
+      const { payload: index } = action;
+      if (state[index]) {
+        if (
+          state[index].name === " Auditorium Hall (Capacity:200)" &&
+          state[index].quantity >= 3
+        ) {
+          return;
+        }
+        state[index].quantity++;
+      }
+    },
+    decrementQuantity: (state, action) => {
+      const { payload: index } = action;
+      if (state[index] && state[index].quantity > 0) {
+        state[index].quantity--;
+      }
+    },
+  },
+});
+
+export const { incrementQuantity, decrementQuantity } = venueSlice.actions;
+
+export default venueSlice.reducer;
