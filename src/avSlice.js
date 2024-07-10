00dfb9bfc223498f04776b975b39c19a03d2014b
ngongang <k.ngongang@addon-solution.de>
2024-07-10 17:26:24 +0200

    Conference Event Planner

--- src/avSlice.js
+++ src/avSlice.js
@@ -3,16 +3,53 @@ import { createSlice } from "@reduxjs/toolkit";
 export const avSlice = createSlice({
   name: "av",
   initialState: [
-    
-  ],
+    {
+      img: "src/assets/projector.jpg",
+      name: "Projectors",
+      cost: 200,
+      quantity: 0,
+    },
+    {
+      img: "src/assets/speakers.jpg",
+      name: "Speaker",
+      cost: 35,
+      quantity: 0,
+    },
+    {
+      img: "src/assets/microphone.jpg",
+      name: "Microphones",
+      cost: 45,
+      quantity: 0,
+    },
+    {
+      img: "src/assets/whiteboard.png",
+      name: "Whiteboards",
+      cost: 80,
+      quantity: 0,
+    },
 
+    {
+      img: "src/assets/signage.jpg",
+      name: "Signage",
+      cost: 80,
+      quantity: 0,
+    },
+  ],
 
   reducers: {
     incrementAvQuantity: (state, action) => {
-      
+
+      const item = state[action.payload];
+      if (item) {
+        item.quantity++;
+      }
     },
     decrementAvQuantity: (state, action) => {
-     
+      
+      const item = state[action.payload];
+      if (item && item.quantity > 0) {
+        item.quantity--;
+      }
     },
   },
 });
